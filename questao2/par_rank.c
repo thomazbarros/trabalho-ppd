853e80507742999347aa2b735d1d8cead839f9d0
thomazavila <thomazavila@c4662fd4-c99e-fa30-751c-00b6e1db43c9>
2010-10-12 15:57:00 +0000

    Início da paralelizacao.

--- questao2/par_rank.c
+++ questao2/par_rank.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "pvm3.h"
 
-/* Rank sort sequencial*/
-int main (void){
-	int tam, i, j, rank;
-	int *numeros;
-	int *rankeados;
+
+/* Rank sort paralelizado*/
+int main (int argc, char *argv[]){
+	int tam, i, j, rank, id, tag, processos;
+	int *numeros, *rankeados, *tids;
+	char *grupo = "grupo";
+
 	srand(time(NULL));
+
+	printf("Digite numero de processos a serem disparados:\n");
+	scanf("%d", &processos);
+
 	printf("Digite o tamanho do vetor a ser ordenado:\n");
 	scanf("%d", &tam );
+
+
 	numeros=(int *) malloc (tam*sizeof(int));
 	rankeados=(int*) malloc (tam*sizeof(int));
-	if((!numeros)||(!rankeados))
+	tids=(int*) malloc (processos*sizeof(int));
+
+
+	if((!numeros)||(!rankeados)||(!tids))
 	{
 		printf("Erro na alocacao de memoria\n");
 		return 1;
 	}
+
 
 	printf("Vetor não ordenado:\n");
 	printf("Números: ");
@@ -27,7 +40,38 @@ int main (void){
 		
 	}
 	printf("\n");
-	for(i=0;i<tam;i++)
+	
+	id = pvm_joingroup(grupo);
+	if(id==0)
+	{
+		if(pvm_spawn("tarefas", NULL, PvmTaskDefault, "", processos-1,tids)!=(processos-1))
+		{
+			printf("Erro: Não foi possível disparar os processos\n");
+			return 2;
+		}
+		else
+		{
+			
+	  		for(i=0;i<tam;i++)
+	              	{
+              			rank=0;
+		                 for(j=0;j<tam;j++)
+          		          {
+                    			if((numeros[i]<numeros[j]) || ((numeros[i]==numeros[j])&&(j<i)))
+                       			{
+                               			 rank++;
+					}
+			 	  }
+		                  rankeados[rank]=numeros[i];
+	        	          printf("rank:%d ", rank);
+			}
+        //		pvm_barrier(grupo, NPROCS);
+	
+		}
+			
+	}
+
+/*	for(i=0;i<tam;i++)
 	{
 		rank=0;
 		for(j=0;j<tam;j++)
@@ -40,7 +84,8 @@ int main (void){
 		rankeados[rank]=numeros[i];
 		printf("rank:%d ", rank);
 	}
-	
+*/	
+
 	printf("Vetor ordenado:\n");
 	printf("Números: ");
 	for(i=0;i<tam;i++)
@@ -49,6 +94,7 @@ int main (void){
 		
 	}
 	printf("\n");
+
 	free(numeros);
 	free(rankeados);
 	return 0;
